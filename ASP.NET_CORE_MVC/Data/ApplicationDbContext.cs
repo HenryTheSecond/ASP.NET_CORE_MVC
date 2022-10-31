12890cf047d7318e52211d872709793f182f5895
Tuyen Vu Cong <intern.vctuyen@tma.com.vn>
2022-10-31 17:56:08 +0700

    first commit

---
+++ ASP.NET_CORE_MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using ASP.NET_CORE_MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NET_CORE_MVC.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {}
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
